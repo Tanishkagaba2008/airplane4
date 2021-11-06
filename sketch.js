a78386d405202b3e539bb85acd5c75e1556e7323
Tanishkagaba2008 <84225116+Tanishkagaba2008@users.noreply.github.com>
2021-11-06 13:54:28 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,166 @@
+var PLAY=1
+var END=0
+var gamestate=PLAY
+
+var bg, bgImg,cloud2Img,airplaneImg,birdImg
+var bottomGround
+var topGround
+var balloon, balloonImg,resetImg,reset,gameover,gameoverImg
+
+function preload(){
+bgImg = loadImage("assets/background.png")
+cloud2Img = loadImage("assets/cloud2.png")
+airplaneImg = loadImage("assets/airplane.png")
+birdImg = loadImage("assets/obsTop2.png")
+resetImg = loadImage("assets/reset2.png")
+gameoverImg = loadImage("assets/gameover.png")
+
+//balloonImg = loadAnimation("assets/balloon1.png","assets/balloon2.png","assets/balloon3.png")
+}
+
+function setup(){
+
+  createCanvas(1500,800);
+//background image
+//bg = createSprite();
+//bg.addImage(bgImg);
+//bg.scale = 4
+
+//creating top and bottom grounds
+
+bottomGround = createSprite(100,700,2900,20);
+bottomGround.visible = true;
+bottomGround.shapeColor="blue"
+
+topGround = createSprite(200,10,800,20);
+topGround.visible = false;
+      
+//creating balloon     
+balloon = createSprite(100,200,20,50);
+balloon.addImage(airplaneImg)
+//ballon.addAnimation("balloon",balloonImg);
+balloon.scale = 0.3;
+
+cloudgroup = new Group()
+birdgroup = new Group()
+
+reset = createSprite(750,460)
+reset.addImage(resetImg)
+reset.scale=0.8
+
+gameover = createSprite(640,400)
+gameover.addImage(gameoverImg)
+gameover.scale=0.10
+
+}
+
+function draw() {
+  
+  background(bgImg);
+
+  if (gamestate===PLAY){
+
+    reset.visible=false
+    gameover.visible=false
+
+    if(keyDown("RIGHT_ARROW")) {
+      balloon.x+=5 ;
+      
+    }
+    if(keyDown("LEFT_ARROW")) {
+      balloon.x-=5 ;
+      
+    }
+
+    if(keyDown("UP_ARROW")) {
+      balloon.y-=5 ;
+      
+    }
+
+    if(keyDown("DOWN_ARROW")) {
+      balloon.y+=5 ;
+      
+    }
+    spawnclouds()
+    spawnbirds()
+
+    if(cloudgroup.isTouching(balloon)||(birdgroup.isTouching(balloon))){
+      gamestate = END
+      
+    
+    }
+     
+    //cloudgroup.setVelocityXEach(0);
+
+    
+    
+    }
+    if(gamestate===END){
+      //textSize(39)
+      //fill("red")
+      //text("GAMEOVER!",640,400)
+      cloudgroup.setVelocityXEach(0)
+      birdgroup.setVelocityXEach(0)
+      cloudgroup.setLifetimeEach(-1)
+      birdgroup.setLifetimeEach(-1)
+      reset.visible=true
+      gameover.visible=true
+
+
+      if(mousePressedOver(reset)) {
+        restart();
+      }
+
+    }
+          
+   
+        drawSprites();
+        
+}
+function spawnclouds(){
+  if (frameCount % 60 === 0) {
+    cloud = createSprite(1300,100,40,10);
+   cloud.y = Math.round(random(50,300));
+   cloud.addImage(cloud2Img);
+   cloud.scale = 0.5;
+   cloud.velocityX = -10;
+   
+    //assign lifetime to the variable
+   cloud.lifetime = 234;
+   
+   //adjust the depth
+   cloud.depth = balloon.depth;
+  balloon.depth = balloon.depth + 1;
+   
+
+   //adding cloud to the group
+  cloudgroup.add(cloud);
+   }
+}
+function spawnbirds(){
+  if (frameCount % 60 === 0) {
+    cloud1 = createSprite(1500,100,40,10);
+   cloud1.y = Math.round(random(30,300));
+   cloud1.addImage(birdImg);
+   cloud1.scale = 0.2;
+   cloud1.velocityX = -10;
+   
+    //assign lifetime to the variable
+   cloud1.lifetime = 234;
+   
+   //adjust the depth
+   cloud1.depth = balloon.depth;
+   balloon.depth = balloon.depth + 1;
+   
+   //adding cloud to the group
+  birdgroup.add(cloud1);
+  }
+}
+function restart(){
+  gamestate=PLAY;
+  console.log("reset")
+  .visible=false
+  reset.visible=false
+  cloudgroup.destroyEach()
+  birdgroup.destroyEach()
+}
